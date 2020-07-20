1a26edd060de0dc5bd6e453b2ee2c69db220f8d3
Andy Tran <antr900@gmail.com>
2020-07-20 09:25:23 -0700

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # potato-game
-coding exercise
+
+
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import PotatoeGame from './game';
+console.log("Webpack is working!")
+const canvas = document.getElementById('potatoe-game');
+new PotatoeGame(canvas);
