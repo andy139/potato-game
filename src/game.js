c570b48238b1e33de078a7d6c97563990e1f4385
Andy Tran <antr900@gmail.com>
2020-07-20 10:47:44 -0700

    💫 Add background using Canvas API
    Add background using Canvas

--- src/game.js
+++ src/game.js
@@ -1,6 +1,26 @@
+import Level from './level';
+
+
 export default class PotatoeGame {
-  constructor(canvas){
-    this.ctx = canvas.getContext("2d");
+  constructor(canvas) {
+    // Inputted html canvas dom element
+    this.ctx = canvas.getContext("2d"); // Allow us to draw shape on page
     this.dimensions = { width: canvas.width, height: canvas.height };
+    this.restart();
   }
+
+  restart() {
+    this.level = new Level(this.dimensions)
+    this.animate();
+
+  }
+
+
+  animate() {
+    this.level.animate(this.ctx);
+    
+  }
+
+
+
 }
--- src/level.js
+++ src/level.js
@@ -3,6 +3,15 @@ export default class Level {
     this.dimensions = dimensions;
   }
 
+  // Responsible for drawing background and obstacles "knives, fryer, innout logo" for potatoe, also responsible for control logic 
+  // for how obstacles move and generated
+
+  // Utilize animate method to control and move obstacles, calculate score, and move potatoe (make new frame)
+
+  animate(ctx) {
+    this.drawBackground(ctx)
+  }
+
   drawBackground(ctx) {
     ctx.fillStyle = "skyblue";
     ctx.fillRect(0, 0, this.dimensions.width, this.dimensions.height);
